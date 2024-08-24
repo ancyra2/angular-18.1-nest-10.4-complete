f90a91250a1884a445eb371ee3846fc7e4897f46
Talih Duran <duran.talih@gmail.com>
2024-08-24 20:40:32 +0300

    feat: add Simple usage CRUD with mysql TypeORM

--- backend/src/cats/cats.service.ts
+++ backend/src/cats/cats.service.ts
@@ -1,26 +1,43 @@
 import { Injectable } from '@nestjs/common';
 import { CreateCatDto } from './dto/create-cat.dto';
 import { UpdateCatDto } from './dto/update-cat.dto';
+import { Cat } from './entities/cat.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class CatsService {
-  create(createCatDto: CreateCatDto) {
-    return 'This action adds a new cat';
+  constructor(
+    @InjectRepository(Cat)
+    private catsRepository: Repository<Cat>,
+  ){}
+  
+  async create(createCatDto: CreateCatDto): Promise<Cat>{
+    const cat = this.catsRepository.create(createCatDto);
+    return await this.catsRepository.save(cat);
   }
 
-  findAll() {
-    return `This action returns all cats`;
+  async findAll(): Promise<Cat[]>{
+    return this.catsRepository.find()
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} cat`;
+  async findOne(id: number) {
+    const cat = await this.catsRepository.findOneBy({id});
+    if (!cat) {
+      throw new Error(`Cat with id ${id} not found`); 
+    }
+    return cat;
   }
 
-  update(id: number, updateCatDto: UpdateCatDto) {
-    return `This action updates a #${id} cat`;
+  async update(id: number, updateCatDto: UpdateCatDto): Promise<Cat> {
+    await this.catsRepository.update(id, updateCatDto);
+    return this.findOne(id); 
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} cat`;
+  async remove(id: number): Promise<void> {
+    const result = await this.catsRepository.delete(id);
+    if (result.affected === 0) {
+      throw new Error(`Cat with id ${id} not found`); 
+    }
   }
 }
